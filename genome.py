bfbae4f8fff689c9cd62edfae812940ba61ea872
Brian Connelly <bdc@bconnelly.net>
2014-07-28 12:37:34 -0700

    Added function to calculate Hamming distance

--- genome.py
+++ genome.py
@@ -3,3 +3,9 @@ import numpy as np
 def bitstring_as_base10(a):
     return np.sum(a * 2**np.arange(len(a))[::-1])
 
+def hamming_distance(a, b):
+    """Compute the Hamming distance (number of bits that differ in the binary
+    representation) between two integers"""
+
+    return bin(a ^ b)[2:].count('1')
+
