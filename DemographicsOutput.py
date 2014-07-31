0e2aae4f6a0628908a763d4b014f0d9be6c4bdd9
Brian Connelly <bdc@bconnelly.net>
2014-07-31 10:45:10 -0700

    Minor code formatting update

--- DemographicsOutput.py
+++ DemographicsOutput.py
@@ -5,6 +5,7 @@ import csv
 
 import Population
 
+
 class DemographicsOutput(object):
 
     def __init__(self, metapopulation, filename='demographics.csv.bz2', delimiter=','):
@@ -18,17 +19,19 @@ class DemographicsOutput(object):
     def update(self, time):
         for n, d in self.metapopulation.topology.nodes_iter(data=True):
             size = len(d['population'])
-            num_producers = d['population'].num_producers()
-            num_nonproducers = size - num_producers
 
             if size == 0:
+                num_producers = 0
+                num_nonproducers = 0
                 prop_producers = 'NA'
                 prop_nonproducers = 'NA'
+                average_fitness = 'NA'
             else:
+                num_producers = d['population'].num_producers()
+                num_nonproducers = size - num_producers
                 prop_producers = 1.0*num_producers/size
                 prop_nonproducers = 1.0*num_nonproducers/size
-
-            average_fitness = d['population'].average_fitness()
+                average_fitness = d['population'].average_fitness()
 
             self.writer.writerow([time, n, size, num_producers, prop_producers,
                                   num_nonproducers, prop_nonproducers,
--- GenotypesOutput.py
+++ GenotypesOutput.py
@@ -8,6 +8,7 @@ import numpy as np
 import Population
 import genome
 
+
 class GenotypesOutput(object):
 
     def __init__(self, metapopulation, filename='demographics.csv.bz2', delimiter=','):
@@ -23,11 +24,10 @@ class GenotypesOutput(object):
     def update(self, time):
         abundances = []
         abundances = np.array([d['population'].abundances for n, d in self.metapopulation.topology.nodes_iter(data=True)])
-        av = np.average(abundances,0)
+        av = np.average(abundances, 0)
         
         for i in range(2**(self.genome_length+1)):
             isprod = genome.is_producer(i, self.genome_length)
             genotype = i & 2**(self.genome_length)-1
             self.writer.writerow([time, genotype, av[i], isprod])
-
 
