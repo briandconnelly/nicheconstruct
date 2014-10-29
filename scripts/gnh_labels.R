ff6f6df021a29c3c8291b57c51df54e4d9a378b8
Brian Connelly <bdc@bconnelly.net>
2014-10-29 16:01:12 -0700

    Added mutation rate analysis, central labels

---
+++ scripts/gnh_labels.R
@@ -0,0 +1,9 @@
+label_time <- 'Time (T)'
+label_genome_length <- 'Genome Length (L)'
+label_migration_rate <- 'Migration Rate (m)'
+label_producer_proportion <- 'Producer Proportion'
+
+label_mu <- expression(paste('Mutation Rate (', mu[p], ', ', mu[s], ')'))
+label_socialmu <- expression(paste('Mutation Rate at Social Locus (', mu[p], ')'))
+
+mutation_labels <- c('1e-7'='0.0000001', '1e-6'='0.000001', '1e-5'='0.00001', '1e-4'='0.0001', '1e-3'='0.001', '1e-2'='0.01', '1e-1'='0.1')
