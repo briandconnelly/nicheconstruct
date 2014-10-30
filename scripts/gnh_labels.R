1cc831c8dcdd42bb2af0e0963a935172092eb7a4
Brian Connelly <bdc@bconnelly.net>
2014-10-30 13:11:09 -0700

    Added analysis of stress mutation rate data

--- scripts/gnh_labels.R
+++ scripts/gnh_labels.R
@@ -2,10 +2,12 @@ label_time <- 'Time (T)'
 label_genome_length <- 'Genome Length (L)'
 label_migration_rate <- 'Migration Rate (m)'
 label_producer_proportion <- 'Producer Proportion'
-label_stressmu <- expression(bold(paste('Survival Mutation Rate (', mu[e], ')')))
+label_stressmu <- expression(bold(paste('Survival Mutation Rate (', mu[t], ')')))
 
 label_mu <- expression(bold(paste('Mutation Rate (', mu[p], ', ', mu[s], ')')))
 label_socialmu <- expression(bold(paste('Mutation Rate at Social Locus (', mu[p], ')')))
+
+label_producer_proportion_sd <- expression(bold(paste(sigma, '(Proportion of Producers Surviving)')))
 
 # TODO: highlight base values in labels with bold text or asterisk
 
