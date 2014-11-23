e3fd22aeacf20d2490edc933aa37e99c0ee00ef5
Brian Connelly <bdc@bconnelly.net>
2014-11-22 17:05:59 -0800

    Linted Metapopulation

--- model/Metapopulation.py
+++ model/Metapopulation.py
@@ -1,25 +1,39 @@
 # -*- coding: utf-8 -*-
 
+"""Represent metapopulations: collections of populations of individuals and the
+migration between them"""
+
 import os
 
 import networkx as nx
 import numpy as np
 
-import genome
-from Population import Population
-import topology
 from DemographicsOutput import DemographicsOutput
 from FitnessOutput import FitnessOutput
+from Population import Population
+import topology
 
 
 class Metapopulation(object):
+    """Define a Metapopulation object
+
+    Metapopulations are collections of populations of individuals.
+
+    """
 
     def __init__(self, config):
         """Initialize a Metapopulation object"""
         self.config = config
         self.time = 0
 
+        # Keep some information about the population
+        self._dirty = None
+        self._size = None
+        self._num_producers = None
+        self._prop_producers = None
+
         # Build the topology, which links the subpopulations
+        self.topology = None
         self.build_topology()
 
         # Create each of the populations
@@ -36,27 +50,28 @@ class Metapopulation(object):
         self.population_construction = config.getboolean(section='Population',
                                                          option='enable_construction')
 
-        for n, d in self.topology.nodes_iter(data=True):
-            d['population'] = Population(metapopulation=self, config=config)
+        for node, data in self.topology.nodes_iter(data=True):
+            data['population'] = Population(metapopulation=self, config=config)
 
             if initial_state == 'corners':
                 # Place all producers in one corner and all non-producers in
                 # the other
-                if n == 0:
-                    d['population'].abundances[2**genome_length_max] = max_cap
-                    d['population'].dilute()
-                elif n == len(self.topology)-1:
-                    d['population'].abundances[0] = min_cap
-                    d['population'].dilute()
+                if node == 0:
+                    data['population'].abundances[2**genome_length_max] = max_cap
+                    data['population'].dilute()
+                elif node == len(self.topology)-1:
+                    data['population'].abundances[0] = min_cap
+                    data['population'].dilute()
 
             elif initial_state == 'stress':
-                cap = int(min_cap + ( (max_cap - min_cap) * initial_producer_proportion))
+                cap = int(min_cap + ((max_cap - min_cap) * \
+                                     initial_producer_proportion))
                 num_producers = int(cap * initial_producer_proportion)
                 num_nonproducers = cap - num_producers
 
-                d['population'].abundances[0] = num_producers
-                d['population'].abundances[2**genome_length_max] = num_nonproducers
-                d['population'].bottleneck(survival_rate=mutation_rate_tolerance)
+                data['population'].abundances[0] = num_producers
+                data['population'].abundances[2**genome_length_max] = num_nonproducers
+                data['population'].bottleneck(survival_rate=mutation_rate_tolerance)
 
         # Get the settings for migration among populations
         self.migration_rate = self.config.getfloat(section='Metapopulation',
@@ -90,6 +105,7 @@ class Metapopulation(object):
                                         filename=os.path.join(data_dir, 'fitness.csv.bz2'))
             self.log_objects.append(out_fitness)
 
+
         self.set_dirty()
 
 
@@ -100,23 +116,8 @@ class Metapopulation(object):
         if prop_producers == 'NA':
             res = "Metapopulation: Size {s}, NA% producers".format(s=self.size())
         else:
-            maxfit = self.max_fitnesses()
-            # TODO get the maximum fitnesses among producers and non-producers
-            maxfit_p = 99
-            maxfit_np = 99
-
-            if maxfit_p > maxfit_np:
-                symbol = '>'
-            elif maxfit_p < maxfit_np:
-                symbol = '<'
-            else:
-                symbol = '='
-
-          #  res = "Metapopulation: Size {s}, {p:.1%} producers. w(P): {mp:.2} "\
-          #        "{sym} w(Np): {mnp:.2}.".format(s=self.size(), p=self.prop_producers(),
-          #                                       mp=maxfit_p, mnp=maxfit_np, sym=symbol)
-
-            res = "Metapopulation: Size {s}, TODO".format(s=self.size())
+            res = "Metapopulation: Size {s}, {p:.1%} "\
+                  "producers".format(s=self.size(), p=self.prop_producers())
 
         return res
 
@@ -206,44 +207,38 @@ class Metapopulation(object):
         section of the configuration file.
         """
 
-        for n, d in self.topology.nodes_iter(data=True):
-            d['population'].dilute()
+        for node, data in self.topology.nodes_iter(data=True):
+            data['population'].dilute()
 
     def grow(self):
         """Grow the metapopulation ...."""
-        for n, d in self.topology.nodes_iter(data=True):
-            d['population'].grow()
+        for node, data in self.topology.nodes_iter(data=True):
+            data['population'].grow()
 
     def mutate(self):
         """Mutate the metapopulation ...."""
-        for n, d in self.topology.nodes_iter(data=True):
-            d['population'].mutate()
+        for node, data in self.topology.nodes_iter(data=True):
+            data['population'].mutate()
 
-    def migrate(self, single_dest=True):
-        """Migrate individuals among the populations
-        
-        * single_dest: if True (default), all migrants will go to a single
-            neighbor population. Otherwise, migrants will be distributed among
-            all neighbors. 
-        
-        """
+    def migrate(self):
+        """Migrate individuals among the populations"""
         if self.migration_rate == 0:
             return
 
-        for n, d in self.topology.nodes_iter(data=True):
-            pop = d['population']
+        for node, data in self.topology.nodes_iter(data=True):
+            pop = data['population']
 
             # Migrate everything to one neighboring population
             if self.migration_dest.lower() == 'single':
                 migrants = pop.select_migrants(migration_rate=self.migration_rate)
-                neighbor_index = np.random.choice(self.topology.neighbors(n))
+                neighbor_index = np.random.choice(self.topology.neighbors(node))
                 neighbor = self.topology.node[neighbor_index]['population']
                 neighbor.add_immigrants(migrants)
                 pop.remove_emigrants(migrants)
             # Distribute the migrants among the neighboring populations
             elif self.migration_dest.lower() == 'neighbors':
-                num_neighbors = self.topology.degree(n)
-                for neighbor_node in self.topology.neighbors_iter(n):
+                num_neighbors = self.topology.degree(node)
+                for neighbor_node in self.topology.neighbors_iter(node):
                     migrants = pop.select_migrants(migration_rate=self.migration_rate/num_neighbors)
                     neighbor = self.topology.node[neighbor_node]['population']
                     neighbor.add_immigrants(migrants)
@@ -252,14 +247,14 @@ class Metapopulation(object):
 
     def census(self):
         """Update each population's abundance to account for migration"""
-        for n, d in self.topology.nodes_iter(data=True):
-            d['population'].census()
+        for node, data in self.topology.nodes_iter(data=True):
+            data['population'].census()
 
 
     def construct(self):
         """Update a population's environment when appropriate"""
-        for n, d in self.topology.nodes_iter(data=True):
-            d['population'].construct()
+        for node, data in self.topology.nodes_iter(data=True):
+            data['population'].construct()
 
 
     def cycle(self):
@@ -305,23 +300,25 @@ class Metapopulation(object):
     def change_environment(self):
         """Change the environment
 
-        The change_environment function changes the environment for the
-        entire metapopulation. This re-generates the fitness landscape and zeros out
-        all fitness-encoding loci. This is meant to represent the metapopulation
-        being subjected to different selective pressures. The number of
-        individuals of each genotype that survive this event are proportional to
-        the abundance of that genotype times the mutation rate (representing
-        individuals that acquired the mutation that allows them to persist).
+        The change_environment function changes the environment for the entire
+        metapopulation. This re-generates the fitness landscape and zeros out
+        all fitness-encoding loci. This is meant to represent the
+        metapopulation being subjected to different selective pressures. The
+        number of individuals of each genotype that survive this event are
+        proportional to the abundance of that genotype times the mutation rate
+        (representing individuals that acquired the mutation that allows them
+        to persist).
+
         """
 
         mutation_rate_tolerance = self.config.getfloat(section='Population',
                                                        option='mutation_rate_tolerance')
 
-        for n, d in self.topology.nodes_iter(data=True):
+        for node, data in self.topology.nodes_iter(data=True):
             # TODO could these 3 steps be encapsulated in a Population-level function?
-            d['population'].bottleneck(survival_rate=mutation_rate_tolerance)
-            d['population'].reset_loci()
-            d['population'].fitness_landscape = d['population'].build_fitness_landscape()
+            data['population'].bottleneck(survival_rate=mutation_rate_tolerance)
+            data['population'].reset_loci()
+            data['population'].fitness_landscape = data['population'].build_fitness_landscape()
 
         self.set_dirty()
 
@@ -381,25 +378,29 @@ class Metapopulation(object):
         """Write any log files"""
 
         if self.time % self.log_frequency == 0:
-            for l in self.log_objects:
-                l.update(time=self.time)
+            for log in self.log_objects:
+                log.update(time=self.time)
 
 
     def cleanup(self):
-        for l in self.log_objects:
-            l.close()
+        """Perform cleanup tasks when the object is cleaned up"""
+        for log in self.log_objects:
+            log.close()
 
 
     def set_dirty(self):
+        """Mark the metapopulation as changed in the current cycle"""
         self._dirty = True
 
 
     def clear_dirty(self):
-        for n, d in self.topology.nodes_iter(data=True):
-            d['population'].clear_dirty()
+        """Mark the metapopulation as unchanged in the current cycle"""
+        for node, data in self.topology.nodes_iter(data=True):
+            data['population'].clear_dirty()
         self._dirty = False
 
 
     def is_dirty(self):
+        """Return whether or not the Metapopulation has been changed"""
         return self._dirty
 
