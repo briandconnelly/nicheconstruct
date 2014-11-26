0cf108e94cd357a1007cc82772551242b66f13b9
Brian Connelly <bdc@bconnelly.net>
2014-11-26 13:17:27 -0800

    Expanded Genotypes Logging
    * GenotypesOutput now includes visible genotype, nonsocial genotype, number of ones, and more
    
    Closes #4

--- model/GenotypesOutput.py
+++ model/GenotypesOutput.py
@@ -4,7 +4,7 @@
 
 import sys
 
-from genome import is_producer
+from genome import is_producer, num_ones
 from OutputWriter import OutputWriter
 
 if sys.version_info[0] == 3:
@@ -19,9 +19,14 @@ class GenotypesOutput(OutputWriter):
     * Time
     * Population
     * Genome length
-    * Genotype (fitness encoding portion)
+    * Genotype
     * IsProducer (0 or 1)
+    * Non-social portion of genotype
+    * Number of Ones
+    * Visible portion of genotype (not including social locus)
+    * Visible number of ones
     * Abundance
+    * Fitness
 
     """
 
@@ -32,17 +37,25 @@ class GenotypesOutput(OutputWriter):
                                               delimiter=delimiter)
 
         self.writer.writerow(['Time', 'Population', 'GenomeLength', 
-                              'Genotype', 'IsProducer', 'Abundance'])
+                              'Genotype', 'IsProducer', 'NonsocialGenotype',
+                              'NumOnes', 'VisibleGenotype', 'VisibleNumOnes',
+                              'Abundance', 'Fitness'])
 
 
     def update(self, time):
         for node, data in self.metapopulation.topology.nodes_iter(data=True):
+            pop = data['population']
+            L = pop.genome_length
+            Lmax = pop.genome_length_max
 
-            L = data['population'].genome_length
-            Lmax = data['population'].genome_length_max
+            for genotype in xrange(pop.abundances.size):
+                visible = genotype & ((2**L) - 1)
+                nonsocial = genotype & (2**Lmax - 1)
 
-            for genotype in xrange(data['population'].abundances.size):
                 self.writer.writerow([time, node, L, genotype,
                                       is_producer(genotype, Lmax),
-                                      data['population'].abundances[genotype]])
+                                      nonsocial, num_ones(nonsocial), visible,
+                                      num_ones(visible),
+                                      pop.abundances[genotype],
+                                      pop.fitness_landscape[genotype]])
 
