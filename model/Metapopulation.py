67089972aab2d51a171a2f424ea6b496b11f4eca
Brian Connelly <bdc@bconnelly.net>
2015-01-15 14:41:24 -0800

    Updates for growth
    * Renamed some config parameters
    * Removed some unused config parameters

--- model/Metapopulation.py
+++ model/Metapopulation.py
@@ -20,22 +20,33 @@ def create_metapopulation(config, topology):
     capacity_max = int(config['Population']['capacity_max'])
     assert 0 <= capacity_min <= capacity_max
 
-    initial_producer_proportion = float(config['Population']['initial_producer_proportion'])
-    assert 0 <= initial_producer_proportion <= 1
+    initial_cooperator_proportion = float(config['Population']['initial_cooperator_proportion'])
+    assert 0 <= initial_cooperator_proportion <= 1
 
     genome_length_min = int(config['Population']['genome_length_min'])
     genome_length_max = int(config['Population']['genome_length_max'])
     assert 0 <= genome_length_min <= genome_length_max
 
     initial_popsize = capacity_min + \
-                  (initial_producer_proportion * (capacity_max - capacity_min))
+                  (initial_cooperator_proportion * (capacity_max - capacity_min))
 
     cols = {'Population': np.repeat(np.arange(size), initial_popsize),
-            'Coop': binomial(1, initial_producer_proportion,
+            'Coop': binomial(1, initial_cooperator_proportion,
                              size*initial_popsize) == 1}
     cols.update({"S{0:02d}".format(i): np.zeros(size*initial_popsize, dtype=np.int) for i in range(1, genome_length_max + 1)})
 
     return pd.DataFrame(cols)
+
+
+def reset_stress_loci(M, Lmax):
+    """Reset all stress loci in the population
+    
+    Resetting a stress locus sets its allelic state to 0.
+    """
+
+    Mcopy = M.copy(deep=True)
+    Mcopy[stress_loci(Lmax)] = 0
+    return Mcopy
 
 
 def mix(M, topology):
@@ -70,10 +81,13 @@ def migrate(M, topology, rate):
 
 def grow(M, config):
     """Grow the population"""
-    # Mcopy.loc[Mcopy.shape[0]] = [NEW_ROW]
 
     Mcopy = M.copy(deep=True)
 
+    base_fitness = float(config['Population']['base_fitness'])
+    cost_cooperation = float(config['Population']['cost_cooperation'])
+    delta = float(config['Population']['benefit_nonzero'])
+    gamma = float(config['Population']['benefit_ordered'])
 
     # Add:
     #Mcopy[Mcopy.index.max() + 1] = [NEW_ROW]
@@ -94,6 +108,8 @@ def grow(M, config):
         # TODO: calculate end density (DEN)
         # TODO: draw DEN-CUR_SIZE samples from the individuals, each with probability PROB
 
+        # IDEA: make a lambda function for calculating fitness that uses S1..S8,Coop
+
         pass
 
 
@@ -104,7 +120,7 @@ def mutate(M, mu_stress, mu_cooperation, Lmax, stress_alleles):
     """Mutate individuals in the metapopulation"""
     assert 0 <= mu_stress <= 1
     assert 0 <= mu_cooperation <= 1
-    assert Lmax > 0
+    assert Lmax >= 0
     assert stress_alleles >= 0
 
     Mcopy = M.copy(deep=True)
