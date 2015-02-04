529adb5ce5f0895be8fb45655f73a57bcae4f50f
Brian Connelly <bdc@bconnelly.net>
2015-02-03 17:17:07 -0800

    Updates

--- model/Metapopulation.py
+++ model/Metapopulation.py
@@ -58,29 +58,27 @@ def assign_fitness(M, config):
     genome_length_max = int(config['Population']['genome_length_max'])
     assert genome_length_max >= 0
 
-    Mcopy = M.copy(deep=True)
-
     stress_columns = stress_colnames(L=genome_length_max)
     stress_alleles = M[stress_columns]
 
     allele_dist = np.apply_along_axis(lambda x: np.bincount(x, minlength=num_stress_alleles+1),
                                       axis=0, arr=stress_alleles)
 
-    Mcopy.Fitness = base_fitness
-    Mcopy.Fitness += np.sum(M[stress_columns] > 0, axis=1) * delta
-    Mcopy.Fitness -= Mcopy.Coop * cost_cooperation
+    M.Fitness = base_fitness
+    M.Fitness += np.sum(M[stress_columns] > 0, axis=1) * delta
+    M.Fitness -= M.Coop * cost_cooperation
 
     if num_stress_alleles > 1 and num_stress_loci > 0:
         # Add gamma times the number of individuals with matching first allele
         # TODO: what if it is all zeros? A bunch of zeros would have higher fitness than delta.
-        Mcopy.Fitness += allele_dist[stress_alleles[stress_columns[0]], 0] * gamma
+        M.Fitness += allele_dist[stress_alleles[stress_columns[0]], 0] * gamma
 
         # Add gamma times the number of individuals with increasing allele value
         stress_alleles_next = (1 + (stress_alleles % num_stress_alleles)).values[:,:-1]
         allele_dist_next = allele_dist[:,1:]
-        Mcopy.Fitness += allele_dist_next[stress_alleles_next, range(genome_length_max-1)].sum(axis=1) * gamma
+        M.Fitness += allele_dist_next[stress_alleles_next, range(genome_length_max-1)].sum(axis=1) * gamma
 
-    return Mcopy
+    return M
 
 
 def reset_stress_loci(M, Lmax):
@@ -89,9 +87,8 @@ def reset_stress_loci(M, Lmax):
     Resetting a stress locus sets its allelic state to 0.
     """
 
-    Mcopy = M.copy(deep=True)
-    Mcopy[stress_colnames(L=Lmax)] = 0
-    return Mcopy
+    M[stress_colnames(L=Lmax)] = 0
+    return M
 
 
 def mix(M, topology):
@@ -101,76 +98,24 @@ def mix(M, topology):
     evenly among the populations
     """
 
-    Mcopy = M.copy(deep=True)
-    Mcopy.Population = np.random.random_integers(low=0, high=len(topology)-1,
-                                                 size=Mcopy.shape[0])
-    return Mcopy
+    M.Population = np.random.random_integers(low=0, high=len(topology)-1,
+                                                 size=M.shape[0])
+    return M
 
 
 def migrate(M, topology, rate):
     """Migrate individuals among subpopulations"""
     assert 0 <= rate <= 1
 
-    Mcopy = M.copy(deep=True)
-
-    emigrants_ix = Mcopy.index[where(binomial(n=1, p=rate,
-                                              size=Mcopy.index.shape[0]) == 1)]
+    emigrants_ix = M.index[where(binomial(n=1, p=rate,
+                                              size=M.index.shape[0]) == 1)]
 
     if emigrants_ix.shape[0] > 0:
         targets = {p: random_neighbor(p, topology) for p in M.Population.unique()}
         get_target = np.vectorize(lambda t: targets[t])
-        Mcopy.loc[emigrants_ix, 'Population'] = get_target(Mcopy.loc[emigrants_ix, 'Population'].values)
+        M.loc[emigrants_ix, 'Population'] = get_target(M.loc[emigrants_ix, 'Population'].values)
 
-    return Mcopy
-
-
-def grow(M, genome_lengths, config):
-    """Grow the population"""
-
-    base_fitness = float(config['Population']['base_fitness'])
-    cost_cooperation = float(config['Population']['cost_cooperation'])
-    delta = float(config['Population']['benefit_nonzero'])
-    gamma = float(config['Population']['benefit_ordered'])
-
-    smin = float(config['Population']['capacity_min'])
-    smax = float(config['Population']['capacity_max'])
-    assert 0 <= smin <= smax
-
-    num_stress_alleles = int(config['Population']['stress_alleles'])
-    assert num_stress_alleles >= 0
-
-    Mcopy = M.copy(deep=True)
-
-    stress_columns = stress_colnames(L=int(config['Population']['genome_length_max']))
-
-    for pop in M.Population.unique():
-        subpop = M[M.Population==pop]
-
-        pct_cooperators = subpop.Coop.mean()
-        num_offspring = int(smin + (pct_cooperators * (smax - smin)) - subpop.shape[0])
-        parent_num_offspring = np.random.multinomial(n=num_offspring,
-                                                     pvals=subpop.Fitness/subpop.Fitness.sum())
-
-        # parent_num_offspring is an array where each element represents a
-        # parent (relative index), and the value contains the number of
-        # offspring
-        offspring = subpop.iloc[np.repeat(np.arange(parent_num_offspring.shape[0]), parent_num_offspring)]
-
-        # Mutate the offspring
-        mu_offspring = mutate(M=offspring,
-                              mu_stress=float(config['Population']['mutation_rate_stress']),
-                              mu_cooperation=float(config['Population']['mutation_rate_cooperation']),
-                              Lmax=int(config['Population']['genome_length_max']),
-                              num_stress_alleles=num_stress_alleles)
-        mu_offspring = assign_fitness(M=mu_offspring, config=config)
-
-        # Merge in the offspring
-        Mcopy = Mcopy.append(mu_offspring)
-
-    # Reindex the metapopulation
-    Mcopy.index = np.arange(Mcopy.shape[0])
-
-    return Mcopy
+    return M
 
 
 def mutate(M, mu_stress, mu_cooperation, Lmax, num_stress_alleles):
@@ -198,4 +143,52 @@ def mutate(M, mu_stress, mu_cooperation, Lmax, num_stress_alleles):
         Mcopy[s] = (Mcopy[s] + (binomial(n=1, p=mu_stress, size=Mcopy[s].shape) * np.random.random_integers(low=1, high=num_stress_alleles, size=Mcopy[s].shape))) % (num_stress_alleles + 1)
 
     return Mcopy
+
+
+def grow(M, genome_lengths, config):
+    """Grow the population"""
+
+    base_fitness = float(config['Population']['base_fitness'])
+    cost_cooperation = float(config['Population']['cost_cooperation'])
+    delta = float(config['Population']['benefit_nonzero'])
+    gamma = float(config['Population']['benefit_ordered'])
+
+    smin = float(config['Population']['capacity_min'])
+    smax = float(config['Population']['capacity_max'])
+    assert 0 <= smin <= smax
+
+    num_stress_alleles = int(config['Population']['stress_alleles'])
+    assert num_stress_alleles >= 0
+
+    stress_columns = stress_colnames(L=int(config['Population']['genome_length_max']))
+
+    for pop in M.Population.unique():
+        subpop = M[M.Population==pop]
+
+        pct_cooperators = subpop.Coop.mean()
+        num_offspring = int(smin + (pct_cooperators * (smax - smin)) - subpop.shape[0])
+        parent_num_offspring = np.random.multinomial(n=num_offspring,
+                                                     pvals=subpop.Fitness/subpop.Fitness.sum())
+
+        # parent_num_offspring is an array where each element represents a
+        # parent (relative index), and the value contains the number of
+        # offspring
+        offspring = subpop.iloc[np.repeat(np.arange(parent_num_offspring.shape[0]), parent_num_offspring)]
+
+        # Mutate the offspring
+        mu_offspring = mutate(M=offspring,
+                              mu_stress=float(config['Population']['mutation_rate_stress']),
+                              mu_cooperation=float(config['Population']['mutation_rate_cooperation']),
+                              Lmax=int(config['Population']['genome_length_max']),
+                              num_stress_alleles=num_stress_alleles)
+        mu_offspring = assign_fitness(M=mu_offspring, config=config)
+
+        # Merge in the offspring
+        M = M.append(mu_offspring)
+
+    # Reindex the metapopulation
+    M.index = np.arange(M.shape[0])
+
+    return M
+
 
