196d94d8c358181d4df8b4a8820b94e4dda029dd
Brian Connelly <bdc@bconnelly.net>
2015-03-26 11:29:09 -0700

    Added Figure 2

--- paper/06_figures.md
+++ paper/06_figures.md
@@ -19,10 +19,13 @@ Proportion of cooperators over time with non-social adaptation
 Proportion of cooperators over time with non-social adaptation and niche construction
 ![Proportion of cooperators over time with non-social adaptation and selective feedbacks](../figures/Figure1C.png)
 
+\newpage
 
 ## Figure 2
 
-Plot showing integral for baseline values, double adaptive benefit (no NC, epsilon=0), no negative NC (L=5, A=5), Extreme negative niche construction (L=1, A=6)
+![Cooperator Presence TODO](../figures/Figure2.png)
+
+\newpage
 
 ## Figure 3
 
@@ -30,25 +33,26 @@ Mean fitness over time for the treatments shown in Figure 2
 
 ### Figure 3A - Fitness for base case: niche construction
 
-![Grand mean Fitness of cooperators and defectors](../figures/Figure3A.png)
+![Grand mean Fitness of cooperators and defectors TODO](../figures/Figure3A.png)
 
 ### Figure 3B - Fitness with double delta, no epsilon
 
-![Grand mean Fitness of cooperators and defectors, double delta, no epsilon](../figures/Figure3B.png)
+![Grand mean Fitness of cooperators and defectors, double delta, no epsilon TODO](../figures/Figure3B.png)
 
 ### Figure 3C - Fitness with no negative niche construction (L=5, A=5)
 
-![Grand mean Fitness of cooperators and defectors, no negative niche construction](../figures/Figure3C.png)
+![Grand mean Fitness of cooperators and defectors, no negative niche construction TODO](../figures/Figure3C.png)
 
 ### Figure 3D - Fitness with extreme negative niche construction (L=1, A=6)
 
-![Grand mean Fitness of cooperators and defectors, extreme negative niche construction](../figures/Figure3D.png)
+![Grand mean Fitness of cooperators and defectors, extreme negative niche construction TODO](../figures/Figure3D.png)
 
-
+\newpage
 
 ## Figure 4
 
 Cooperators invade from single population
+
 
 ## Figure 5
 
---
+++ scripts/figure_2.R
@@ -0,0 +1,47 @@
+#!/usr/bin/env Rscript
+
+# Plot Figure 2
+
+library(magrittr)
+library(dplyr)
+library(ggplot2)
+library(ggplot2bdc)
+
+# Read the data sets
+base_data <- read.csv('../data/L05_A06_1xDelta_1xEpsilon.csv')
+nonc_data <- read.csv('../data/L05_A06_2xDelta_0xEpsilon.csv')
+noneg_data <- read.csv('../data/L05_A05_1xDelta_1xEpsilon.csv')
+neg_data <- read.csv('../data/L01_A06_1xDelta_1xEpsilon.csv')
+
+
+# Combine the data sets
+fig2data <- bind_rows(base_data, nonc_data, noneg_data, neg_data)
+fig2data$Replicate <- as.factor(fig2data$Replicate)
+fig2data$Treatment <- factor(fig2data$Treatment,
+                             levels=c('L05_A06_1xDelta_1xEpsilon',
+                                      'L05_A06_2xDelta_0xEpsilon',
+                                      'L05_A05_1xDelta_1xEpsilon',
+                                      'L01_A06_1xDelta_1xEpsilon'),
+                             labels=c('With Adaptation\nWith Niche Construction',
+                                      'Adaptation,\nWithout\n Niche Construction',
+                                      'Without Negative\nNiche Construction',
+                                      'Extreme Negative\nNiche Construction'))
+
+# Get the area under the curve of cooperator proportion for each replicate of
+# each treatment ("Cooperator Presence")
+fig2integrals <- fig2data %>%
+    group_by(Treatment, Replicate) %>%
+    summarise(N=n(), Integral=sum(CooperatorProportion)/(max(Time)-min(Time)))
+
+# Make the plot
+fig2 <- ggplot(data=fig2integrals, aes(x=Treatment, y=Integral)) +
+    geom_point(shape=1, alpha=0.2) +
+    stat_summary(fun.data='mean_cl_normal') + 
+    #geom_boxplot() +
+    scale_y_continuous(limits=c(0,1)) + 
+    labs(x='', y='Cooperator Presence') +
+    theme_bdc_grey(ticks.x=FALSE) + theme(axis.text.x = element_text(vjust=1, face='bold'))
+fig2 <- rescale_golden(plot=fig2)
+
+ggsave_golden(plot=fig2, filename='../figures/Figure2.png', dpi=300)
+
