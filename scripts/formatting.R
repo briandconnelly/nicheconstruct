b4a20143ddc2911372086a96502148fd0a8057d9
Brian Connelly <bdc@bconnelly.net>
2015-03-27 10:37:14 -0700

    Added Figure 6A

--- scripts/formatting.R
+++ scripts/formatting.R
@@ -3,6 +3,7 @@ label_cooperator_proportion <- 'Cooperator Proportion'
 label_cooperator_presence <- 'Cooperator Presence'
 label_mean_fitness <- 'Mean Fitness'
 label_migrationrate <- 'Migration Rate'
+label_benefit <- 'Benefit of Cooperation'
 
 color_cooperator <- '#729ECE'
 color_defector <- '#ED665D'
