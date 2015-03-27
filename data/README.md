038d0ad0ca3a1179a795e69f0599b26825329ecf
Brian Connelly <bdc@bconnelly.net>
2015-03-27 03:56:43 -0700

    Data Updates

--- data/README.md
+++ data/README.md
@@ -9,5 +9,7 @@
 | L00.csv        | Combined data from simulations where no adaptation was possible |
 | L05_A06_2xDelta_0xEpsilon.csv | Combined data from simulations where the benefits of non-social adaptations are doubled in order to compare against the benefits possible with both non-social adaptation and niche construction |
 | L05_A06_1xDelta_1xEpsilon | Combined data from baseline simulation where non-social adaptation and selective feedback from niche construction are included |
+| L05_A06_1xDelta_0xEpsilon | Combined data from simulations where niche construction was not present |
 | L05_A05_1xDelta_1xEpsilon.csv | Combined data from simulation where negative niche construction was not present |
 | L01_A06_1xDelta_1xEpsilon.csv | Combined data from extreme negative niche construction simulations |
+| migrationrate.csv | Combined data from simulations where migration rate was varied |
--- scripts/figure_6B.R
+++ scripts/figure_6B.R
@@ -16,11 +16,11 @@ base_data$MigrationRate <- 0.05
 base_data2 <- select(base_data, Time, PopulationSize, CooperatorProportion, MinCooperatorFitness, MaxCooperatorFitness, MeanCooperatorFitness, MinDefectorFitness, MaxDefectorFitness, MeanDefectorFitness, ShannonIndex, SimpsonIndex, GenomeLength, Alleles, Delta, Epsilon, MigrationRate, Replicate)
 
 # Combine the data sets
-fig6Adata <- bind_rows(migration_data, base_data2)
+fig6Bdata <- bind_rows(migration_data, base_data2)
 
 # Get the area under the curve of cooperator proportion for each replicate of
 # each treatment ("Cooperator Presence")
-fig6Aintegrals <- fig6Adata %>%
+fig6Bintegrals <- fig6Bdata %>%
     group_by(MigrationRate, Replicate) %>%
     summarise(N=n(), Integral=sum(CooperatorProportion)/(max(Time)-min(Time)))
 
@@ -28,21 +28,21 @@ fig6Aintegrals <- fig6Adata %>%
 
 
 # Make the plot
-fig6A <- ggplot(data=fig6Aintegrals, aes(x=MigrationRate, y=Integral)) +
+fig6B <- ggplot(data=fig6Bintegrals, aes(x=MigrationRate, y=Integral)) +
     geom_point(shape=1, alpha=0.2) +
     stat_summary(fun.data='mean_cl_normal') + 
     scale_x_log10() +
     scale_y_continuous(limits=c(0,1)) + 
     labs(x=label_migrationrate, y=label_cooperator_presence) +
     theme_bdc_grey(grid.y=TRUE)
-fig6A <- rescale_golden(plot=fig6A)
+fig6B <- rescale_golden(plot=fig6B)
 
-#ggsave_golden(plot=fig6A, filename='../figures/Figure6A.png', dpi=300)
+#ggsave_golden(plot=fig6B, filename='../figures/Figure6B.png', dpi=300)
 
-g <- ggplotGrob(fig6A)                                                          
-g <- gtable_add_grob(g, textGrob(expression(bold('A')), gp=gpar(col='black', fontsize=20), x=0, hjust=0, vjust=0.5), t=1, l=2)
+g <- ggplotGrob(fig6B)                                                          
+g <- gtable_add_grob(g, textGrob(expression(bold('B')), gp=gpar(col='black', fontsize=20), x=0, hjust=0, vjust=0.5), t=1, l=2)
 
-png('../figures/Figure6A.png', width=6, height=3.708204, units='in', res=300)   
+png('../figures/Figure6B.png', width=6, height=3.708204, units='in', res=300)   
 grid.newpage()                                                                  
 grid.draw(g)                                                                    
 dev.off()
